d507fe7c3c4de650eebd47508f73b2aa3607cbf9
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2017-06-28 02:40:19 +0700

    fix: node 8 compatibility

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ function avowify(fn) {
   return function (...args) {
     try { fn(...args) }
     catch (e) {
-      throw e.message ? e.message : e
+      /**
+       *  Node.js's assert does not allow passing of Error Objects any more
+       *
+       *  See: https://github.com/nodejs/node/blob/master/lib/assert.js
+       *  As of Node v8.1.2, assert.ok (alias of just assert) is defined as
+       *
+       *    function ok(value, message) {
+       *      if (!value) fail(value, true, message, '==', assert.ok);
+       *    }
+       *
+       *  So, in order to throw custom errors, we refer to the original
+       *  arguments passed into avow.
+       */
+      const lastArg = args[args.length - 1]
+      if (lastArg instanceof Error) throw lastArg
+      throw e
     }
   }
 }
